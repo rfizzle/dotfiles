33deb07d94a9124e43be8887f5a9c409fa3e9a09
Coleton Pierson <coleton.pierson@gmail.com>
2023-10-25 16:54:45 +0700

    chore: updated Brewfile; removed ssh config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,7 @@ set -x PATH $HOME/.bin $PATH
 set -x PATH $HOME/.local/bin $PATH
 set -x GOPATH $HOME/.go
 set -x PATH $PATH $GOPATH/bin
+set -U FZF_LEGACY_KEYBINDINGS 0
 
 if status is-interactive
   # Commands to run in interactive sessions can go here
